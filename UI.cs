851ce481d5ea8c586feba21638a9e80d6b5dd429
mirasl <santaluciamira+chia@gmail.com>
2023-01-10 14:14:27 -0800

    Adds random graph functionality

--- UI.cs
+++ UI.cs
@@ -5,6 +5,23 @@ public class UI : Control
 {
     [Signal] delegate void Graph(string expression, float size, float p);
 
+    string[] sampleEquations = new string[] {
+        "sin(cos(x + t) + (y + t)/2)",
+        "sin(x + t) - sin(y + t)",
+        "x*x - y*y",
+        "sin(cos(sin(cos(x+t) - y) - x) - t)",
+        "1/(x*y)"
+    };
+
+    public override void _PhysicsProcess(float delta)
+    {
+        // LineEdit le = GetNode<LineEdit>("Equation");
+        // if (le.HasFocus())
+        // {
+        //     GD.Print("focused!");
+        // }
+    }
+
     public void sig_GraphButtonPressed()
     {
         if (GetNode<LineEdit>("Equation").Text == "")
@@ -22,5 +39,11 @@ public class UI : Control
                 GetNode<LineEdit>("Equation").Text,
                 float.Parse(size),
                 (float)GetNode<HSlider>("HSlider").Value);
+    }
+
+    public void sig_RandomButtonPressed()
+    {
+        GetNode<LineEdit>("Equation").Text = sampleEquations[(int)(GD.Randf()*sampleEquations.Length)];
+        sig_GraphButtonPressed();
     }
 }
