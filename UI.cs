785bd617500f73bfd3a7bc88179444bbef6f8cf7
mirasl <santaluciamira+chia@gmail.com>
2023-01-12 14:24:53 -0800

    Makes Random button always generate a new graph, and adds more random graphs

--- UI.cs
+++ UI.cs
@@ -7,13 +7,26 @@ public class UI : Control
     [Signal] delegate void SetLineEditFocus(bool state);
 
     private bool lineEditFocused = false;
+    private int lastRandom = 0;
 
     string[] sampleEquations = new string[] {
         "sin(cos(x + t) + (y + t)/2)",
         "sin(x + t) - sin(y + t)",
         "x*x - y*y",
         "sin(cos(sin(cos(x+t) - y) - x) - t)",
-        "1/(x*y)"
+        "1/(x*y)",
+        "sin(x*x - y*y)",
+        "sin(x*x/10 + y*y/10 + t)",
+        "sin(x*x + y*y)",
+        "((4/10)^2-((6/10)-(x^2+y^2)^(1/2))^2)^(1/2)",
+        "sin((x+15)^(6/10) + t) - cos((y+15)^(6/10) + t)",
+        "1/((x^2+y^2))",
+        "(x^2+y^2)^(1/2)",
+        "(x^2+y^2)^(1/2) * sin(t)",
+        "1/(100*sin(x) + 100*cos(y))",
+        "x*x*y*y",
+        "cos(t)*x",
+        "((2)^2-(5-(x^2+y^2)^(1/2))^2)^(1/2) + x*sin(t) + y*cos(t)"
     };
 
     public override void _PhysicsProcess(float delta)
@@ -49,6 +62,15 @@ public class UI : Control
     public void sig_RandomButtonPressed()
     {
         GetNode<Button>("Random").ReleaseFocus();
+        
+        // Guarentees next random graph is different than the last:
+        int random = (int)(GD.Randf() * sampleEquations.Length);
+        while (random == lastRandom) 
+        {
+            random = (int)(GD.Randf() * sampleEquations.Length);
+        }
+        lastRandom = random;
+
         GetNode<LineEdit>("Equation").Text = sampleEquations[(int)(GD.Randf() *
                 sampleEquations.Length)];
         sig_GraphButtonPressed();
